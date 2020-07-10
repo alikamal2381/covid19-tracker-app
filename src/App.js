baad9c8a3ed04df3952219eef58a3af1287a6c29
AliKamal <alikamal2381@gmail.com>
2020-07-11 00:59:44 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from "react";
-import { Cards, Chart, CountryPicker } from "./components";
-//import styles from "./App.module.css";
 import "./App.css";
-import { fetchData } from "./api";
+import Details from "./Details";
 
 class App extends Component {
-  state = {
-    data: {},
-  };
-  async componentDidMount() {
-    const fetchData1 = await fetchData();
-
-    this.setState({ data: fetchData1 });
-    //console.log(data);
-  }
-
   render() {
-    const { data } = this.state;
+    //const { data } = this.state;
     return (
-      <div className="container">
-        <h1>Covid-19 Tracker App</h1>
-        <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+      <div className="App">
+        <h1>COVID19 Tracker App by Ali Kamal</h1>
+        <Details />
       </div>
     );
   }
